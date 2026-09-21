2b7a97c8ecf3b569da7125fa3e7032d921c9684d
Blubblub09 <124732911+Blubblub09@users.noreply.github.com>
2026-09-21 12:06:41 +0000

    Completed Navbar and Added Footer section

---
+++ showcase_website/script.js
@@ -0,0 +1,13 @@
+function darkMode() {
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark-mode") ? "dark" : "light"
+  );
+}
+
+window.addEventListener("load", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+});
